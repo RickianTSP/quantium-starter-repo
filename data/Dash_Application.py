c6600c378680ba2d4a627f1900da131cdfa4fa36
Eric Wong <ericwong973@hotmail.com>
2022-07-08 06:11:56 +1000

    added radio buttons and applied some css

--- data/Dash_Application.py
+++ data/Dash_Application.py
@@ -1,34 +1,43 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import csv
 import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
 
-with open("./data/Filtered_Data.csv") as csv_file_r:
-    csv_reader = csv.reader(csv_file_r, delimiter=',')
-    date = []
-    sales = []
-    for row in csv_reader:
-        if row:
-            date.append(row[1])
-            sales.append(row[0])
-
-df = pd.DataFrame({
-    "Date": date,
-    "Sales": sales,
-})
-
-fig = px.line(df, x="Date", y="Sales")
+df = pd.read_csv('Filtered_Data.csv')
+df = df.sort_values(by="Date")
 
 app.layout = html.Div(children=[
-    html.H1(children='Sales ($) vs Date'),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    html.Div([
+        html.H1(children='Sales($) vs Date'),
+        dcc.RadioItems(
+            ['north', 'east', 'south', 'west', 'all'],
+            'All',
+            id='region',
+            inline=True
+            )
+        ], style = {'text-align': 'center'}),
+    
+        html.Div([
+            dcc.Graph(
+                id='output_graph',
+            )
+        ])
 ])
+
+@app.callback(
+    Output('output_graph', 'figure'),
+    Input('region', 'value'))
+def update_graph(region_name):
+    if region_name != 'all':
+        filtered_df = df[df['Region'] == region_name]
+    else :
+        filtered_df = df
+
+    fig = px.line(filtered_df, x="Date", y="Sales")
+    fig.update_layout(transition_duration=500)
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
